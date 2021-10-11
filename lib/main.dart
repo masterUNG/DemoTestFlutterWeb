0a3f310de6d6abedbfebd3d35830ffe62e842595
masterUNG <phrombutr@gmail.com>
2021-10-11 10:26:35 +0700

    Success Firebase

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:dashbord/states/acc_problem.dart';
+import 'package:dashbord/states/chiangmai.dart';
+import 'package:dashbord/states/dashbord.dart';
+import 'package:dashbord/states/home.dart';
+import 'package:dashbord/states/manage_order.dart';
+import 'package:dashbord/states/setting_acc.dart';
+import 'package:dashbord/states/success_order.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  await Firebase.initializeApp().then((value) {
+    print('Firebase initial Success');
+    runApp(const MyApp());
+  }).catchError((error) {
+    print('Firebase Error ==>> ${error.toString()}');
+    //  runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Dashboard(),
+    );
+  }
+}
